20897ce4c83c32c6d437acf3417dd5740e82635a
iwoodruff <isaac.woodruff@gmail.com>
2014-10-18 16:27:35 -0400

    cleaning up so much cruft

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,79 +13,6 @@ angular.module('portfolioApp')
       ctrl.gridCaptions = data;
     });
 
-    ctrl.activeBox = {};
-
-    var activeDescription = undefined,
-        i = 0,
-        length
-
-    ctrl.activateBox = function (y) {
-      if (y.description) {
-        y.toggled = true;
-
-        var activeBox = angular.copy(y);
-
-        var letters = typeof activeBox.description == 'string' ? activeBox.description.split('') : activeBox.description;
-
-        var caption = [];
-
-        if (letters.length > 25) {
-          var z = -12
-        }
-        
-        var lineBreak = ''
-
-        var shiftRight = 0,
-            shiftLeft
-
-        if (letters.length > 25) {
-          shiftLeft = -1 * Math.floor(letters.indexOf(' ', 25) / 2);
-          // shiftRight = letters.indexOf(' ', 25) / 2
-        } else {
-          shiftLeft = -1 * Math.floor(letters.length / 2);
-          // shiftRight = 0
-        }
-
-        for (var x = 0; x < letters.length; x++) {
-          var letter = {};
-          letter.letter = letters[x];
-
-          if (x > 25 && letters[x] == ' ' && !lineBreak) {
-            lineBreak = 'line-break_';
-
-            // z = -1 * Math.floor(letters.length / 2);
-            shiftLeft = Math.floor((x - letters.length) / 2);
-            shiftRight = 0;
-          }
-
-          if (shiftLeft < 0) {
-            letter.index = lineBreak + shiftLeft;
-
-            shiftLeft++;
-          } else {
-            letter.index = lineBreak + shiftRight;
-
-            shiftRight++;
-          }
-
-          caption.push(letter);
-        }
-
-        activeBox.description = caption;
-
-        ctrl.activeBox = activeBox;
-
-        ctrl.gridCaptions[y.column + 1][y.row + 1].toggled = true;
-        ctrl.gridCaptions[y.column + 1][y.row - 1].toggled = true;
-        ctrl.gridCaptions[y.column - 1][y.row - 1].toggled = true;
-        ctrl.gridCaptions[y.column - 1][y.row + 1].toggled = true;
-        ctrl.gridCaptions[y.column][y.row - 1].toggled = true;
-        ctrl.gridCaptions[y.column][y.row + 1].toggled = true;
-        ctrl.gridCaptions[y.column - 1][y.row].toggled = true;
-        ctrl.gridCaptions[y.column + 1][y.row].toggled = true;
-      }
-    };
-
     ctrl.rotators = [
       'portfolio_gallery/shoe/shoe1.png',
       'portfolio_gallery/shoe/shoe2.png',
@@ -96,15 +23,4 @@ angular.module('portfolioApp')
       'portfolio_gallery/shoe/shoe7.png',
       'portfolio_gallery/shoe/shoe8.png',
     ];
-
-    var gridCaptions = [],
-        j = 0
-
-
-
-    ctrl.index = 0;
-
-    ctrl.updateIndex = function (idx) {
-      ctrl.index = idx;
-    };
   });
--- app/scripts/directives/shoeZoom.js
+++ app/scripts/directives/shoeZoom.js
@@ -5,150 +5,222 @@
 portfolioApp.directive('shoeZoom', ['$timeout', '$q', function ($timeout, $q) {
   return {
     restrict : 'E',
+    controllerAs : 'shoeZoom',
     templateUrl : 'views/partials/shoeDisplay.html',
     link : function (scope, element, attrs) {
-      var browserWindow = angular.element(window),
-          body = angular.element('body'),
-          windowHeight = (browserWindow.innerHeight() * 0.80),
-          imgContainer = angular.element(element.find('.img-container')),
-          containerLrg = angular.element(element.find('.img-lrg')),
-          imgLrg = angular.element(element.find('.img-lrg img')),
-          captionContainer = angular.element(element.find('.caption-container')),
-          zoomView = angular.element(element.find('.img-zoom')),
-          imgZoom = angular.element(element.find('.img-zoom img')),
-          heightMinusNav = windowHeight - angular.element('nav').height(),
-          widthZoomView = parseInt(zoomView.css('width')),
-          heightZoomView = parseInt(zoomView.css('height')),
-          activeRotator,
-          parallaxX,
-          diffContainerWidth,
-          diffImgWidth,
-          diffImgHeight,
-          percentageX,
-          percentageY,
-          heightLrg,
-          widthLrg,
-          containerWidth,
-          containerHeight,
-          heightZoom,
-          widthZoom,
-          offset,
-          offsetX,
-          offsetY,
-          relativeX,
-          relativeY,
-          imgX,
-          imgY,
-          zoomX,
-          zoomY,
-          options;
-
-      scope.zoomLoaded = false;
-
-      options = !!attrs.shoeZoomOptions && scope.$eval(attrs.shoeZoomOptions);
+      var options = !!attrs.shoeZoomOptions && scope.$eval(attrs.shoeZoomOptions);
       scope.imgDefault = options.default;
       scope.imgZoom = options.zoom;
+    },
+    controller : function ($scope, $element, $interval) {
+      var ctrl = this,
+          browserWindow = angular.element(window),
+          imgContainer = $element.find('.img-container')[0],
+          containerLrg = $element.find('.img-lrg')[0],
+          imgLrg = $element.find('.img-lrg img')[0],
+          imgZoom = $element.find('.img-zoom img')[0],
+          zoomView = $element.find('.img-zoom')[0],
+          widthZoomView = zoomView.offsetWidth,
+          heightZoomView = zoomView.offsetHeight,
+          containerHeight,
+          containerWidth,
+          diffContainerWidth,
+          heightLrg,
+          widthLrg,
+          heightZoom,
+          widthZoom,
+          relativeX,
+          relativeY,
+          percentageX,
+          percentageY,
+          parallaxX,
+          zoomX,
+          zoomY,
+          imgX,
+          imgY,
+          activeRotator,
+          widthToHeight,
+          totalX = 70,
+          totalY,
+          boxHeight,
+          boxWidth
 
-      imgLrg.bind('load', function () {
-        heightLrg = parseInt(imgLrg.css('height'));
-        widthLrg = parseInt(imgLrg.css('width'));
+      ctrl.lrgLoaded = false;
+      ctrl.showZoom = false;
+      ctrl.imgZoom = {};
+      ctrl.imgContainer = {};
+      ctrl.gridContianer = {};
+
+
+      var windowDimensions = function () {
+        return {
+          h : browserWindow.height(),
+          w : browserWindow.width()
+        };
+      };
+
+      var orientElements = function () {
+        ctrl.lrgLoaded = true;
+
+        heightLrg = imgLrg.offsetHeight;
+        widthLrg = imgLrg.offsetWidth;
         heightZoom = heightLrg * 1.4;
+        widthZoom = widthLrg * 1.4;
 
-        imgContainer.css({
-          'height' : heightLrg * 0.85
-        });
+        ctrl.imgContainer = {
+          'height' : heightLrg * 0.85 + 'px'
+        };
 
-        imgZoom.css({
-          'height' : heightZoom
-        });
+        ctrl.imgZoom = {
+          'height' : heightZoom + 'px'
+        };
 
-        browserWindow.bind('resize', function () {
-          heightLrg = parseInt(imgContainer.css('height'));
-          heightZoom = heightLrg * 1.4;
+        ctrl.gridContainer = {
+          'height' : heightLrg,
+          'width' : widthLrg
+        };
 
-          imgContainer.css({
-            'height' : heightLrg
-          });
-
-          imgZoom.css({
-            'height' : heightZoom
-          });
-        });
-
-        containerWidth = parseInt(imgContainer.css('width'));
-        containerHeight = parseInt(imgContainer.css('height'));
+        containerHeight = heightLrg * 0.85;
+        containerWidth = imgContainer.offsetWidth;
 
         diffContainerWidth = containerWidth - widthLrg;
 
-        // TODO: move all this jquery nonsense to the dom.
+        widthToHeight = widthLrg / heightLrg;
+        totalY = Math.ceil(totalX / widthToHeight);
+        boxHeight = widthLrg / totalX;
+        boxWidth = heightLrg / totalY;
+      };
 
-        imgZoom.bind('load', function () {
-          widthZoom = parseInt(imgZoom.css('width'));
-          diffImgWidth = parseInt(imgLrg.css('width')) - widthZoom;
-          diffImgHeight = containerHeight - heightZoom;
+      ctrl.gridBox = function (box) {
+        return {
+          'height' : boxHeight + 'px',
+          'top' : boxHeight * box.row,
+          'left' : boxWidth * box.column
+        };
+      };
 
+      $scope.$watch(windowDimensions, function (newVal, oldVal) {
+      }, true);
 
-          // moving this to the dom will more easily prevent knock-on event handler registration.
-          imgContainer.bind('mousemove', function (e) {
+      angular.element(imgLrg).on('load', orientElements);
 
-            offset = offset || imgContainer.offset();
-            offsetX = offsetX || offset.left;
-            offsetY = offsetY || offset.top;
+      ctrl.mousemove = function (event) {
+        relativeX = event.pageX - imgContainer.offsetLeft;
+        relativeY = event.pageY - imgContainer.offsetTop;
 
-            relativeX = e.pageX - offsetX;
-            relativeY = e.pageY - offsetY;
+        percentageX = relativeX / containerWidth;
+        percentageY = relativeY / containerHeight;
 
-            percentageX = relativeX / containerWidth;
-            percentageY = relativeY / containerHeight;
+        if (percentageX > 0.06 && percentageX < 0.3) {
+          activeRotator = 1;
+        } else if (percentageX > 0.3 && percentageX < 0.43) {
+          activeRotator = 0;
+        } else if (percentageX > 0.43 && percentageX < 0.5) {
+          activeRotator = 7;
+        } else if (percentageX > 0.5 && percentageX < 0.56) {
+          activeRotator = 6;
+        } else if (percentageX > 0.56 && percentageX < 0.64) {
+          activeRotator = 5;
+        } else if (percentageX > 0.64 && percentageX < 0.8) {
+          activeRotator = 4;
+        } else if (percentageX > 0.8 && percentageX < 0.94) {
+          activeRotator = 3;
+        }
 
-            if (percentageX > 0.06 && percentageX < 0.3) {
-              activeRotator = 1;
-            } else if (percentageX > 0.3 && percentageX < 0.43) {
-              activeRotator = 0;
-            } else if (percentageX > 0.43 && percentageX < 0.5) {
-              activeRotator = 7;
-            } else if (percentageX > 0.5 && percentageX < 0.56) {
-              activeRotator = 6;
-            } else if (percentageX > 0.56 && percentageX < 0.64) {
-              activeRotator = 5;
-            } else if (percentageX > 0.64 && percentageX < 0.8) {
-              activeRotator = 4;
-            } else if (percentageX > 0.8 && percentageX < 0.94) {
-              activeRotator = 3;
+        if (activeRotator != ctrl.activeRotator) {
+          ctrl.activeRotator = activeRotator;
+        }
+
+        parallaxX = percentageX * diffContainerWidth;
+
+        ctrl.containerLrg = {
+          'webkitTransform' : 'translate3D(' + parallaxX + 'px, 0px, 0px)'
+        };
+
+        // magic. I'm consistently amazed that this works. I have no idea how.
+
+        zoomX = -1 * percentageX * widthZoomView;
+        zoomY = -1 * percentageY * heightZoomView;
+
+        ctrl.captionContainer = {
+          'webkitTransform' : 'translate3D(' + (relativeX + zoomX) + 'px, ' + (relativeY + zoomY) + 'px, 0px)'
+        };
+
+        imgX = percentageX * (widthZoomView - widthZoom);
+        imgY = percentageY * (300 - heightZoom);
+
+        ctrl.imgZoom['webkitTransform'] = 'translate3D(' + imgX + 'px, ' + imgY + 'px, 0px)';
+      };
+
+      var gridCaptions,
+          activeBox,
+          letters,
+          caption,
+          lineBreak,
+          shiftRight,
+          shiftLeft,
+          letter,
+          x
+
+      $scope.$watch('Main.gridCaptions', function (newVal, oldVal) {
+        if (!!newVal && !oldVal){
+          gridCaptions = newVal;
+        }
+      });
+
+      ctrl.activateBox = function (box) {
+        if (box.description) {
+          box.toggled = true;
+
+          activeBox = angular.copy(box);
+
+          letters = typeof activeBox.description == 'string' ? activeBox.description.split('') : activeBox.description;
+
+          caption = [];
+
+          lineBreak = '';
+
+          shiftRight = 0;
+          shiftLeft = letters.length > 25 ? (-1 * Math.floor(letters.indexOf(' ', 25) / 2)) : (-1 * Math.floor(letters.length / 2));
+
+          for (x = 0; x < letters.length; x++) {
+            letter = {};
+            letter.letter = letters[x];
+
+            if (x > 25 && letters[x] == ' ' && !lineBreak) {
+              lineBreak = 'line-break_';
+
+              shiftLeft = Math.floor((x - letters.length) / 2);
+              shiftRight = 0;
             }
 
-            scope.$apply(scope.activeRotator = activeRotator);
+            if (shiftLeft < 0) {
+              letter.index = lineBreak + shiftLeft;
+              shiftLeft++;
+            } else {
+              letter.index = lineBreak + shiftRight;
+              shiftRight++;
+            }
+
+            caption.push(letter);
+          }
+
+          activeBox.description = caption;
+
+          ctrl.activeBox = activeBox;
+
+          gridCaptions[activeBox.column + 1][activeBox.row + 1].toggled = true;
+          gridCaptions[activeBox.column + 1][activeBox.row - 1].toggled = true;
+          gridCaptions[activeBox.column - 1][activeBox.row - 1].toggled = true;
+          gridCaptions[activeBox.column - 1][activeBox.row + 1].toggled = true;
+          gridCaptions[activeBox.column][activeBox.row - 1].toggled = true;
+          gridCaptions[activeBox.column][activeBox.row + 1].toggled = true;
+          gridCaptions[activeBox.column - 1][activeBox.row].toggled = true;
+          gridCaptions[activeBox.column + 1][activeBox.row].toggled = true;
+        }
+      };
 
 
-            parallaxX = percentageX * diffContainerWidth;
-
-            containerLrg.css({
-              'webkitTransform' : 'translate3D(' + parallaxX + 'px, 0px, 0px)'
-            });
-
-
-            zoomX = -1 * percentageX * widthZoomView;
-            zoomY = -1 * percentageY * heightZoomView;
-
-            captionContainer.css({
-              'webkitTransform' : 'translate3D(' + (relativeX + zoomX) + 'px, ' + (relativeY + zoomY) + 'px, 0px)'
-            });
-
-            imgX = percentageX * (widthZoomView - widthZoom);
-            imgY = percentageY * (300 - heightZoom);
-
-            imgZoom.css({
-              'webkitTransform' : 'translate3D(' + imgX + 'px, ' + imgY + 'px, 0px)'
-            });
-          });
-
-          scope.$apply( function () {
-            scope.zoomLoaded = true;
-          });
-        });
-      });
-    },
-    controller : function ($scope, $element, $interval) {
       var gridCaptions,
           activeBox,
           regenerateGrid,
@@ -156,15 +228,10 @@ portfolioApp.directive('shoeZoom', ['$timeout', '$q', function ($timeout, $q) {
           x,
           y
 
-      console.log($element.find('.img-lrg')[0].offsetWidth)
-
-      $scope.showZoom = false;
-
-      $scope.regenerateGrid = function () {
-        gridCaptions = $scope.$parent.Main.gridCaptions;
+      ctrl.regenerateGrid = function () {
+        ctrl.showZoom = !ctrl.showZoom;
 
         regenerate = function (toggle) {
-
           x = 0,
           y = 0;
 
@@ -183,8 +250,6 @@ portfolioApp.directive('shoeZoom', ['$timeout', '$q', function ($timeout, $q) {
 
               if (!!toggle) {
                 regenerate(false);
-
-                $scope.showZoom = !$scope.showZoom;
               }
             }
           }, 3);
